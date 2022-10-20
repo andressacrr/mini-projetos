172f9fe4aa15f3a64a9a9419efcca376e3d6ef91
Andressa Carolina Rodrigues <andressacrodrigues1998@hotmail.com>
2022-10-19 21:18:32 -0300

    adicionando classe Principal em (projetoIngresso)

--- projetoIngresso/src/empresa/Principal.java
+++ projetoIngresso/src/empresa/Principal.java
@@ -3,9 +3,13 @@ package empresa;
 public class Principal {
     public static void main(String[] args) {
 
+        Ingresso i = new Ingresso("Hopi hari", 100);
+            System.out.println(i.toString());
 
 
-
-
+        IngressoVip iv = new IngressoVip("Hopi hari", 100, 50);
+              System.out.println();
+              System.out.println(i.toString());
+              System.out.println(iv.toString());
     }
 }
